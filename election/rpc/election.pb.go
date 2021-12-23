63942a37609b2227a4771c1faef2babf63ec80ee
DongGua <597449675@qq.com>
2021-12-24 00:21:55 +0800

    pb 生成

--- election/rpc/election.pb.go
+++ election/rpc/election.pb.go
@@ -2,15 +2,11 @@
 // versions:
 // 	protoc-gen-go v1.27.1
 // 	protoc        v3.19.0
-// source: election.proto
+// source: election/election.proto
 
 package election_grpc
 
 import (
-	context "context"
-	grpc "google.golang.org/grpc"
-	codes "google.golang.org/grpc/codes"
-	status "google.golang.org/grpc/status"
 	protoreflect "google.golang.org/protobuf/reflect/protoreflect"
 	protoimpl "google.golang.org/protobuf/runtime/protoimpl"
 	reflect "reflect"
@@ -36,7 +32,7 @@ type OathRequest struct {
 func (x *OathRequest) Reset() {
 	*x = OathRequest{}
 	if protoimpl.UnsafeEnabled {
-		mi := &file_election_proto_msgTypes[0]
+		mi := &file_election_election_proto_msgTypes[0]
 		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
 		ms.StoreMessageInfo(mi)
 	}
@@ -49,7 +45,7 @@ func (x *OathRequest) String() string {
 func (*OathRequest) ProtoMessage() {}
 
 func (x *OathRequest) ProtoReflect() protoreflect.Message {
-	mi := &file_election_proto_msgTypes[0]
+	mi := &file_election_election_proto_msgTypes[0]
 	if protoimpl.UnsafeEnabled && x != nil {
 		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
 		if ms.LoadMessageInfo() == nil {
@@ -62,7 +58,7 @@ func (x *OathRequest) ProtoReflect() protoreflect.Message {
 
 // Deprecated: Use OathRequest.ProtoReflect.Descriptor instead.
 func (*OathRequest) Descriptor() ([]byte, []int) {
-	return file_election_proto_rawDescGZIP(), []int{0}
+	return file_election_election_proto_rawDescGZIP(), []int{0}
 }
 
 func (x *OathRequest) GetNodeId() string {
@@ -91,7 +87,7 @@ type OathReply struct {
 func (x *OathReply) Reset() {
 	*x = OathReply{}
 	if protoimpl.UnsafeEnabled {
-		mi := &file_election_proto_msgTypes[1]
+		mi := &file_election_election_proto_msgTypes[1]
 		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
 		ms.StoreMessageInfo(mi)
 	}
@@ -104,7 +100,7 @@ func (x *OathReply) String() string {
 func (*OathReply) ProtoMessage() {}
 
 func (x *OathReply) ProtoReflect() protoreflect.Message {
-	mi := &file_election_proto_msgTypes[1]
+	mi := &file_election_election_proto_msgTypes[1]
 	if protoimpl.UnsafeEnabled && x != nil {
 		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
 		if ms.LoadMessageInfo() == nil {
@@ -117,7 +113,7 @@ func (x *OathReply) ProtoReflect() protoreflect.Message {
 
 // Deprecated: Use OathReply.ProtoReflect.Descriptor instead.
 func (*OathReply) Descriptor() ([]byte, []int) {
-	return file_election_proto_rawDescGZIP(), []int{1}
+	return file_election_election_proto_rawDescGZIP(), []int{1}
 }
 
 func (x *OathReply) GetNodeId() string {
@@ -143,7 +139,7 @@ type HeartRequest struct {
 func (x *HeartRequest) Reset() {
 	*x = HeartRequest{}
 	if protoimpl.UnsafeEnabled {
-		mi := &file_election_proto_msgTypes[2]
+		mi := &file_election_election_proto_msgTypes[2]
 		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
 		ms.StoreMessageInfo(mi)
 	}
@@ -156,7 +152,7 @@ func (x *HeartRequest) String() string {
 func (*HeartRequest) ProtoMessage() {}
 
 func (x *HeartRequest) ProtoReflect() protoreflect.Message {
-	mi := &file_election_proto_msgTypes[2]
+	mi := &file_election_election_proto_msgTypes[2]
 	if protoimpl.UnsafeEnabled && x != nil {
 		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
 		if ms.LoadMessageInfo() == nil {
@@ -169,7 +165,7 @@ func (x *HeartRequest) ProtoReflect() protoreflect.Message {
 
 // Deprecated: Use HeartRequest.ProtoReflect.Descriptor instead.
 func (*HeartRequest) Descriptor() ([]byte, []int) {
-	return file_election_proto_rawDescGZIP(), []int{2}
+	return file_election_election_proto_rawDescGZIP(), []int{2}
 }
 
 type HeartReply struct {
@@ -181,7 +177,7 @@ type HeartReply struct {
 func (x *HeartReply) Reset() {
 	*x = HeartReply{}
 	if protoimpl.UnsafeEnabled {
-		mi := &file_election_proto_msgTypes[3]
+		mi := &file_election_election_proto_msgTypes[3]
 		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
 		ms.StoreMessageInfo(mi)
 	}
@@ -194,7 +190,7 @@ func (x *HeartReply) String() string {
 func (*HeartReply) ProtoMessage() {}
 
 func (x *HeartReply) ProtoReflect() protoreflect.Message {
-	mi := &file_election_proto_msgTypes[3]
+	mi := &file_election_election_proto_msgTypes[3]
 	if protoimpl.UnsafeEnabled && x != nil {
 		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
 		if ms.LoadMessageInfo() == nil {
@@ -207,55 +203,56 @@ func (x *HeartReply) ProtoReflect() protoreflect.Message {
 
 // Deprecated: Use HeartReply.ProtoReflect.Descriptor instead.
 func (*HeartReply) Descriptor() ([]byte, []int) {
-	return file_election_proto_rawDescGZIP(), []int{3}
+	return file_election_election_proto_rawDescGZIP(), []int{3}
 }
 
-var File_election_proto protoreflect.FileDescriptor
+var File_election_election_proto protoreflect.FileDescriptor
 
-var file_election_proto_rawDesc = []byte{
-	0x0a, 0x0e, 0x65, 0x6c, 0x65, 0x63, 0x74, 0x69, 0x6f, 0x6e, 0x2e, 0x70, 0x72, 0x6f, 0x74, 0x6f,
-	0x12, 0x07, 0x73, 0x65, 0x72, 0x76, 0x69, 0x63, 0x65, 0x22, 0x3a, 0x0a, 0x0b, 0x4f, 0x61, 0x74,
-	0x68, 0x52, 0x65, 0x71, 0x75, 0x65, 0x73, 0x74, 0x12, 0x17, 0x0a, 0x07, 0x6e, 0x6f, 0x64, 0x65,
-	0x5f, 0x69, 0x64, 0x18, 0x01, 0x20, 0x01, 0x28, 0x09, 0x52, 0x06, 0x6e, 0x6f, 0x64, 0x65, 0x49,
-	0x64, 0x12, 0x12, 0x0a, 0x04, 0x74, 0x65, 0x72, 0x6d, 0x18, 0x02, 0x20, 0x01, 0x28, 0x0d, 0x52,
-	0x04, 0x74, 0x65, 0x72, 0x6d, 0x22, 0x3c, 0x0a, 0x09, 0x4f, 0x61, 0x74, 0x68, 0x52, 0x65, 0x70,
-	0x6c, 0x79, 0x12, 0x17, 0x0a, 0x07, 0x6e, 0x6f, 0x64, 0x65, 0x5f, 0x69, 0x64, 0x18, 0x01, 0x20,
-	0x01, 0x28, 0x09, 0x52, 0x06, 0x6e, 0x6f, 0x64, 0x65, 0x49, 0x64, 0x12, 0x16, 0x0a, 0x06, 0x61,
-	0x63, 0x63, 0x65, 0x70, 0x74, 0x18, 0x02, 0x20, 0x01, 0x28, 0x08, 0x52, 0x06, 0x61, 0x63, 0x63,
-	0x65, 0x70, 0x74, 0x22, 0x0e, 0x0a, 0x0c, 0x48, 0x65, 0x61, 0x72, 0x74, 0x52, 0x65, 0x71, 0x75,
-	0x65, 0x73, 0x74, 0x22, 0x0c, 0x0a, 0x0a, 0x48, 0x65, 0x61, 0x72, 0x74, 0x52, 0x65, 0x70, 0x6c,
-	0x79, 0x32, 0x7c, 0x0a, 0x0f, 0x65, 0x6c, 0x65, 0x63, 0x74, 0x69, 0x6f, 0x6e, 0x53, 0x65, 0x72,
-	0x76, 0x69, 0x63, 0x65, 0x12, 0x32, 0x0a, 0x04, 0x4f, 0x61, 0x74, 0x68, 0x12, 0x14, 0x2e, 0x73,
-	0x65, 0x72, 0x76, 0x69, 0x63, 0x65, 0x2e, 0x4f, 0x61, 0x74, 0x68, 0x52, 0x65, 0x71, 0x75, 0x65,
-	0x73, 0x74, 0x1a, 0x12, 0x2e, 0x73, 0x65, 0x72, 0x76, 0x69, 0x63, 0x65, 0x2e, 0x4f, 0x61, 0x74,
-	0x68, 0x52, 0x65, 0x70, 0x6c, 0x79, 0x22, 0x00, 0x12, 0x35, 0x0a, 0x05, 0x48, 0x65, 0x61, 0x72,
-	0x74, 0x12, 0x15, 0x2e, 0x73, 0x65, 0x72, 0x76, 0x69, 0x63, 0x65, 0x2e, 0x48, 0x65, 0x61, 0x72,
-	0x74, 0x52, 0x65, 0x71, 0x75, 0x65, 0x73, 0x74, 0x1a, 0x13, 0x2e, 0x73, 0x65, 0x72, 0x76, 0x69,
-	0x63, 0x65, 0x2e, 0x48, 0x65, 0x61, 0x72, 0x74, 0x52, 0x65, 0x70, 0x6c, 0x79, 0x22, 0x00, 0x42,
-	0x11, 0x5a, 0x0f, 0x2e, 0x3b, 0x65, 0x6c, 0x65, 0x63, 0x74, 0x69, 0x6f, 0x6e, 0x5f, 0x67, 0x72,
-	0x70, 0x63, 0x62, 0x06, 0x70, 0x72, 0x6f, 0x74, 0x6f, 0x33,
+var file_election_election_proto_rawDesc = []byte{
+	0x0a, 0x17, 0x65, 0x6c, 0x65, 0x63, 0x74, 0x69, 0x6f, 0x6e, 0x2f, 0x65, 0x6c, 0x65, 0x63, 0x74,
+	0x69, 0x6f, 0x6e, 0x2e, 0x70, 0x72, 0x6f, 0x74, 0x6f, 0x12, 0x07, 0x73, 0x65, 0x72, 0x76, 0x69,
+	0x63, 0x65, 0x22, 0x3a, 0x0a, 0x0b, 0x4f, 0x61, 0x74, 0x68, 0x52, 0x65, 0x71, 0x75, 0x65, 0x73,
+	0x74, 0x12, 0x17, 0x0a, 0x07, 0x6e, 0x6f, 0x64, 0x65, 0x5f, 0x69, 0x64, 0x18, 0x01, 0x20, 0x01,
+	0x28, 0x09, 0x52, 0x06, 0x6e, 0x6f, 0x64, 0x65, 0x49, 0x64, 0x12, 0x12, 0x0a, 0x04, 0x74, 0x65,
+	0x72, 0x6d, 0x18, 0x02, 0x20, 0x01, 0x28, 0x0d, 0x52, 0x04, 0x74, 0x65, 0x72, 0x6d, 0x22, 0x3c,
+	0x0a, 0x09, 0x4f, 0x61, 0x74, 0x68, 0x52, 0x65, 0x70, 0x6c, 0x79, 0x12, 0x17, 0x0a, 0x07, 0x6e,
+	0x6f, 0x64, 0x65, 0x5f, 0x69, 0x64, 0x18, 0x01, 0x20, 0x01, 0x28, 0x09, 0x52, 0x06, 0x6e, 0x6f,
+	0x64, 0x65, 0x49, 0x64, 0x12, 0x16, 0x0a, 0x06, 0x61, 0x63, 0x63, 0x65, 0x70, 0x74, 0x18, 0x02,
+	0x20, 0x01, 0x28, 0x08, 0x52, 0x06, 0x61, 0x63, 0x63, 0x65, 0x70, 0x74, 0x22, 0x0e, 0x0a, 0x0c,
+	0x48, 0x65, 0x61, 0x72, 0x74, 0x52, 0x65, 0x71, 0x75, 0x65, 0x73, 0x74, 0x22, 0x0c, 0x0a, 0x0a,
+	0x48, 0x65, 0x61, 0x72, 0x74, 0x52, 0x65, 0x70, 0x6c, 0x79, 0x32, 0x7c, 0x0a, 0x0f, 0x65, 0x6c,
+	0x65, 0x63, 0x74, 0x69, 0x6f, 0x6e, 0x53, 0x65, 0x72, 0x76, 0x69, 0x63, 0x65, 0x12, 0x32, 0x0a,
+	0x04, 0x4f, 0x61, 0x74, 0x68, 0x12, 0x14, 0x2e, 0x73, 0x65, 0x72, 0x76, 0x69, 0x63, 0x65, 0x2e,
+	0x4f, 0x61, 0x74, 0x68, 0x52, 0x65, 0x71, 0x75, 0x65, 0x73, 0x74, 0x1a, 0x12, 0x2e, 0x73, 0x65,
+	0x72, 0x76, 0x69, 0x63, 0x65, 0x2e, 0x4f, 0x61, 0x74, 0x68, 0x52, 0x65, 0x70, 0x6c, 0x79, 0x22,
+	0x00, 0x12, 0x35, 0x0a, 0x05, 0x48, 0x65, 0x61, 0x72, 0x74, 0x12, 0x15, 0x2e, 0x73, 0x65, 0x72,
+	0x76, 0x69, 0x63, 0x65, 0x2e, 0x48, 0x65, 0x61, 0x72, 0x74, 0x52, 0x65, 0x71, 0x75, 0x65, 0x73,
+	0x74, 0x1a, 0x13, 0x2e, 0x73, 0x65, 0x72, 0x76, 0x69, 0x63, 0x65, 0x2e, 0x48, 0x65, 0x61, 0x72,
+	0x74, 0x52, 0x65, 0x70, 0x6c, 0x79, 0x22, 0x00, 0x42, 0x11, 0x5a, 0x0f, 0x2e, 0x3b, 0x65, 0x6c,
+	0x65, 0x63, 0x74, 0x69, 0x6f, 0x6e, 0x5f, 0x67, 0x72, 0x70, 0x63, 0x62, 0x06, 0x70, 0x72, 0x6f,
+	0x74, 0x6f, 0x33,
 }
 
 var (
-	file_election_proto_rawDescOnce sync.Once
-	file_election_proto_rawDescData = file_election_proto_rawDesc
+	file_election_election_proto_rawDescOnce sync.Once
+	file_election_election_proto_rawDescData = file_election_election_proto_rawDesc
 )
 
-func file_election_proto_rawDescGZIP() []byte {
-	file_election_proto_rawDescOnce.Do(func() {
-		file_election_proto_rawDescData = protoimpl.X.CompressGZIP(file_election_proto_rawDescData)
+func file_election_election_proto_rawDescGZIP() []byte {
+	file_election_election_proto_rawDescOnce.Do(func() {
+		file_election_election_proto_rawDescData = protoimpl.X.CompressGZIP(file_election_election_proto_rawDescData)
 	})
-	return file_election_proto_rawDescData
+	return file_election_election_proto_rawDescData
 }
 
-var file_election_proto_msgTypes = make([]protoimpl.MessageInfo, 4)
-var file_election_proto_goTypes = []interface{}{
+var file_election_election_proto_msgTypes = make([]protoimpl.MessageInfo, 4)
+var file_election_election_proto_goTypes = []interface{}{
 	(*OathRequest)(nil),  // 0: service.OathRequest
 	(*OathReply)(nil),    // 1: service.OathReply
 	(*HeartRequest)(nil), // 2: service.HeartRequest
 	(*HeartReply)(nil),   // 3: service.HeartReply
 }
-var file_election_proto_depIdxs = []int32{
+var file_election_election_proto_depIdxs = []int32{
 	0, // 0: service.electionService.Oath:input_type -> service.OathRequest
 	2, // 1: service.electionService.Heart:input_type -> service.HeartRequest
 	1, // 2: service.electionService.Oath:output_type -> service.OathReply
@@ -267,13 +264,13 @@ var file_election_proto_depIdxs = []int32{
 	0, // [0:0] is the sub-list for field type_name
 }
 
-func init() { file_election_proto_init() }
-func file_election_proto_init() {
-	if File_election_proto != nil {
+func init() { file_election_election_proto_init() }
+func file_election_election_proto_init() {
+	if File_election_election_proto != nil {
 		return
 	}
 	if !protoimpl.UnsafeEnabled {
-		file_election_proto_msgTypes[0].Exporter = func(v interface{}, i int) interface{} {
+		file_election_election_proto_msgTypes[0].Exporter = func(v interface{}, i int) interface{} {
 			switch v := v.(*OathRequest); i {
 			case 0:
 				return &v.state
@@ -285,7 +282,7 @@ func file_election_proto_init() {
 				return nil
 			}
 		}
-		file_election_proto_msgTypes[1].Exporter = func(v interface{}, i int) interface{} {
+		file_election_election_proto_msgTypes[1].Exporter = func(v interface{}, i int) interface{} {
 			switch v := v.(*OathReply); i {
 			case 0:
 				return &v.state
@@ -297,7 +294,7 @@ func file_election_proto_init() {
 				return nil
 			}
 		}
-		file_election_proto_msgTypes[2].Exporter = func(v interface{}, i int) interface{} {
+		file_election_election_proto_msgTypes[2].Exporter = func(v interface{}, i int) interface{} {
 			switch v := v.(*HeartRequest); i {
 			case 0:
 				return &v.state
@@ -309,7 +306,7 @@ func file_election_proto_init() {
 				return nil
 			}
 		}
-		file_election_proto_msgTypes[3].Exporter = func(v interface{}, i int) interface{} {
+		file_election_election_proto_msgTypes[3].Exporter = func(v interface{}, i int) interface{} {
 			switch v := v.(*HeartReply); i {
 			case 0:
 				return &v.state
@@ -326,134 +323,18 @@ func file_election_proto_init() {
 	out := protoimpl.TypeBuilder{
 		File: protoimpl.DescBuilder{
 			GoPackagePath: reflect.TypeOf(x{}).PkgPath(),
-			RawDescriptor: file_election_proto_rawDesc,
+			RawDescriptor: file_election_election_proto_rawDesc,
 			NumEnums:      0,
 			NumMessages:   4,
 			NumExtensions: 0,
 			NumServices:   1,
 		},
-		GoTypes:           file_election_proto_goTypes,
-		DependencyIndexes: file_election_proto_depIdxs,
-		MessageInfos:      file_election_proto_msgTypes,
+		GoTypes:           file_election_election_proto_goTypes,
+		DependencyIndexes: file_election_election_proto_depIdxs,
+		MessageInfos:      file_election_election_proto_msgTypes,
 	}.Build()
-	File_election_proto = out.File
-	file_election_proto_rawDesc = nil
-	file_election_proto_goTypes = nil
-	file_election_proto_depIdxs = nil
-}
-
-// Reference imports to suppress errors if they are not otherwise used.
-var _ context.Context
-var _ grpc.ClientConnInterface
-
-// This is a compile-time assertion to ensure that this generated file
-// is compatible with the grpc package it is being compiled against.
-const _ = grpc.SupportPackageIsVersion6
-
-// ElectionServiceClient is the client API for ElectionService service.
-//
-// For semantics around ctx use and closing/ending streaming RPCs, please refer to https://godoc.org/google.golang.org/grpc#ClientConn.NewStream.
-type ElectionServiceClient interface {
-	Oath(ctx context.Context, in *OathRequest, opts ...grpc.CallOption) (*OathReply, error)
-	Heart(ctx context.Context, in *HeartRequest, opts ...grpc.CallOption) (*HeartReply, error)
-}
-
-type electionServiceClient struct {
-	cc grpc.ClientConnInterface
-}
-
-func NewElectionServiceClient(cc grpc.ClientConnInterface) ElectionServiceClient {
-	return &electionServiceClient{cc}
-}
-
-func (c *electionServiceClient) Oath(ctx context.Context, in *OathRequest, opts ...grpc.CallOption) (*OathReply, error) {
-	out := new(OathReply)
-	err := c.cc.Invoke(ctx, "/service.electionService/Oath", in, out, opts...)
-	if err != nil {
-		return nil, err
-	}
-	return out, nil
-}
-
-func (c *electionServiceClient) Heart(ctx context.Context, in *HeartRequest, opts ...grpc.CallOption) (*HeartReply, error) {
-	out := new(HeartReply)
-	err := c.cc.Invoke(ctx, "/service.electionService/Heart", in, out, opts...)
-	if err != nil {
-		return nil, err
-	}
-	return out, nil
-}
-
-// ElectionServiceServer is the server API for ElectionService service.
-type ElectionServiceServer interface {
-	Oath(context.Context, *OathRequest) (*OathReply, error)
-	Heart(context.Context, *HeartRequest) (*HeartReply, error)
-}
-
-// UnimplementedElectionServiceServer can be embedded to have forward compatible implementations.
-type UnimplementedElectionServiceServer struct {
-}
-
-func (*UnimplementedElectionServiceServer) Oath(context.Context, *OathRequest) (*OathReply, error) {
-	return nil, status.Errorf(codes.Unimplemented, "method Oath not implemented")
-}
-func (*UnimplementedElectionServiceServer) Heart(context.Context, *HeartRequest) (*HeartReply, error) {
-	return nil, status.Errorf(codes.Unimplemented, "method Heart not implemented")
-}
-
-func RegisterElectionServiceServer(s *grpc.Server, srv ElectionServiceServer) {
-	s.RegisterService(&_ElectionService_serviceDesc, srv)
-}
-
-func _ElectionService_Oath_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
-	in := new(OathRequest)
-	if err := dec(in); err != nil {
-		return nil, err
-	}
-	if interceptor == nil {
-		return srv.(ElectionServiceServer).Oath(ctx, in)
-	}
-	info := &grpc.UnaryServerInfo{
-		Server:     srv,
-		FullMethod: "/service.electionService/Oath",
-	}
-	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
-		return srv.(ElectionServiceServer).Oath(ctx, req.(*OathRequest))
-	}
-	return interceptor(ctx, in, info, handler)
-}
-
-func _ElectionService_Heart_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
-	in := new(HeartRequest)
-	if err := dec(in); err != nil {
-		return nil, err
-	}
-	if interceptor == nil {
-		return srv.(ElectionServiceServer).Heart(ctx, in)
-	}
-	info := &grpc.UnaryServerInfo{
-		Server:     srv,
-		FullMethod: "/service.electionService/Heart",
-	}
-	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
-		return srv.(ElectionServiceServer).Heart(ctx, req.(*HeartRequest))
-	}
-	return interceptor(ctx, in, info, handler)
-}
-
-var _ElectionService_serviceDesc = grpc.ServiceDesc{
-	ServiceName: "service.electionService",
-	HandlerType: (*ElectionServiceServer)(nil),
-	Methods: []grpc.MethodDesc{
-		{
-			MethodName: "Oath",
-			Handler:    _ElectionService_Oath_Handler,
-		},
-		{
-			MethodName: "Heart",
-			Handler:    _ElectionService_Heart_Handler,
-		},
-	},
-	Streams:  []grpc.StreamDesc{},
-	Metadata: "election.proto",
+	File_election_election_proto = out.File
+	file_election_election_proto_rawDesc = nil
+	file_election_election_proto_goTypes = nil
+	file_election_election_proto_depIdxs = nil
 }
